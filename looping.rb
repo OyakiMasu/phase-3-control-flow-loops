1f5cd9cb180b82cb4d4125f236c22d923661cdc4
OyakMasu21 <akzaki17@gmail.com>
2023-02-07 13:44:13 +0300

    Completed assignment six of ruby

--- looping.rb
+++ looping.rb
@@ -1,6 +1,16 @@
+require 'pry'
+
 def happy_new_year
-  # your code here
+  counter = 10 
+  until counter == 0
+  puts counter 
+  counter -= 1
+  end
+  puts "Happy New Year!"
 end
+
+# binding.pry
+
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
@@ -15,10 +25,23 @@ def fizzbuzz(num)
   end
 end
 
-def fizzbuzz_printer
-  # your code here
+def fizzbuzz_printer 
+  num = 1 
+  while num <= 100 
+     puts (fizzbuzz(num))
+        num+=1
+  end
+  
 end
 
+
 def reverse_string(str)
-  # your code here
+  reverse_string = ""
+  i=0
+  while i < str.length
+    reverse_string = str[i] + reverse_string
+    i+=1
+  end
+
+return reverse_string  
 end
